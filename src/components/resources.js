602f6a2dcc05cb6ddf5221da99a02bc089067893
Elijah Prince <elijahprince73@gmail.com>
2017-11-20 20:29:20 -0800

    Adding react router

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special
+      <ul>
+        <li>test</li>
+        <li>fsfs</li>
+        <li>sfsfs</li>
+      </ul>
+    </div>
+  )
+}
